8038aef879959ad30040085616b895a35acc4295
s-kamihirase <syun.sora0111@gmail.com>
2025-07-14 18:00:39 +0900

    7/14

---
+++ SpringSample/src/main/java/com/example/demo/login/aspect/LogAspect.java
@@ -0,0 +1,30 @@
+package com.example.demo.login.aspect;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+//ポイント1:@Aspect
+@Aspect
+@Component
+public class LogAspect{
+	
+	//ポイント２：AOPの実装
+	@Around("bean(* Controller)") //PointCut（実行場所） ： Bean
+	
+	public Object startLog(ProceedingJoinPoint jp) throws Throwable{
+		System.out.println("メソッド開始：" + jp.getSignature());
+		
+		try {
+			//メソッド実行
+			Object result = jp.proceed();
+			
+			System.out.println("メソッド終了：" + jp.getSignature());
+			
+			return result;
+		} catch(Exception e) {
+			System.out.println("メソッド異常終了：" + jp.getSignature());
+			e.printStackTrace();
+			throw e;
+		}
+	}
+}
